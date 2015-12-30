d2f2ae36b18d4b4951bc4dace9199d07aa2a1b55
CasaTaloyum <casatwy@msn.com>
2015-12-30 14:38:46 -0600

    first init

---
+++ BSViewToImage/BSViewToImage/UIView+ViewToImage.h
@@ -0,0 +1,15 @@
+//
+//  UIView+ViewToImage.h
+//  BSViewToImage
+//
+//  Created by casa on 15/12/30.
+//  Copyright © 2015年 casa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (ViewToImage)
+
+- (UIImage *)imageWithView:(UIView *)view;
+
+@end
---
+++ BSViewToImage/BSViewToImage/UIView+ViewToImage.m
@@ -0,0 +1,13 @@
+//
+//  UIView+ViewToImage.m
+//  BSViewToImage
+//
+//  Created by casa on 15/12/30.
+//  Copyright © 2015年 casa. All rights reserved.
+//
+
+#import "UIView+ViewToImage.h"
+
+@implementation UIView (ViewToImage)
+
+@end
